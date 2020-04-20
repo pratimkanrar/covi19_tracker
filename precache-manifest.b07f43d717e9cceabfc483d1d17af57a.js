09c32d0b43e38305e01ae6a4cbfec871c80bb3c1
root <root@kali>
2020-04-20 16:25:57 +0530

    Updates

---
+++ precache-manifest.b07f43d717e9cceabfc483d1d17af57a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdc917bbdcd61d0cc7d510dedd46c97",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "44007ddb142ed662fd03",
+    "url": "/covid19_tracker/static/css/main.f3cce8f2.chunk.css"
+  },
+  {
+    "revision": "a6676ffd756be723877b",
+    "url": "/covid19_tracker/static/js/2.3806d4f4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracker/static/js/2.3806d4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44007ddb142ed662fd03",
+    "url": "/covid19_tracker/static/js/main.c8a0a023.chunk.js"
+  },
+  {
+    "revision": "2183b9a547aec37d1c65",
+    "url": "/covid19_tracker/static/js/runtime-main.544c4f78.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19_tracker/static/media/image.d7265326.png"
+  }
+]);
